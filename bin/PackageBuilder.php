f9c997e38f224174aec5d89b2909a4532cc5902b
arajcany <arajcany@users.noreply.github.com>
2023-09-18 08:01:56 +1000

    Proper application packaging

--- bin/PackageBuilder.php
+++ bin/PackageBuilder.php
@@ -6,8 +6,8 @@ ini_set('memory_limit', '2G');
 
 use App\Utility\Releases\BuildTasks;
 
-require __DIR__ . '/config/paths.php';
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/../config/paths.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 $BuildTasks = new BuildTasks();
 $BuildTasks->setAppName('SampleApp');
